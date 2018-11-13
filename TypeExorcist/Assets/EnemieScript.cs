e798de4aea950540e50820bee433da73c5cacc23
Yessica Tamara Servín Domínguez <yessicasd@TR12.CAT>
2018-11-13 13:38:53 +0100

    adding enemy movement

--- TypeExorcist/Assets/EnemieScript.cs
+++ TypeExorcist/Assets/EnemieScript.cs
@@ -6,10 +6,16 @@ public class EnemieScript : MonoBehaviour {
 
 	public
 	string enemyName= "hola";
-	// Use this for initialization
-	void Start () {
-		
-	}
+    GameObject target;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("player");
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 vec = new Vector2(target.transform.position.x-rb.position.x , target.transform.position.y - rb.position.y);
+        vec.Normalize();
+        rb.velocity = vec;
+    }
 	
 	// Update is called once per frame
 	void Update () {
