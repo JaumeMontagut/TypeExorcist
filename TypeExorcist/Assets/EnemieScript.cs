9dbd74b150e2d8951b34cf202e8083e21c75a91d
Yessica Tamara Servín Domínguez <yessicasd@tr12.cat>
2018-11-13 12:29:58 +0100

    adding enemy prefab

---
+++ TypeExorcist/Assets/EnemieScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieScript : MonoBehaviour {
+
+	public
+	string enemyName= "hola";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+};
