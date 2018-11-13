f8686d6a3f964581a67c2a55d953668b47e6cf92
Yessica Tamara Servín Domínguez <yessicasd@tr12.cat>
2018-11-13 12:40:10 +0100

    adding enemy spawn with click

--- TypeExorcist/Assets/GetInput.cs
+++ TypeExorcist/Assets/GetInput.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GetInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject enemy;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            Instantiate(enemy, position, Quaternion.identity);
+        }
+    }
 }
