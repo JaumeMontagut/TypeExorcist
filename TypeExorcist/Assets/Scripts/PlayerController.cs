0b231c823ea1036b225cb9238bea5a264b94c38a
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-11-24 02:21:14 +0100

    Unfocusing enemy
    Color doens't change correctly, needs further investigation on the TextMeshPro component

--- TypeExorcist/Assets/Scripts/PlayerController.cs
+++ TypeExorcist/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
@@ -14,6 +15,11 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UnfocusEnemy();
+        }
+
         for (KeyCode currKey = KeyCode.A; currKey < KeyCode.Z + 1; currKey++)
         {
             if (Input.GetKeyDown(currKey))
@@ -23,23 +29,39 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    private void FocusEnemy(Enemy enemyToFocus)
+    {
+        focusedEnemy = enemyToFocus;
+        if (focusedEnemy != null)
+        {
+            focusedEnemy.text.color = new Color(255, 255, 255, 255);
+            //focusedEnemy.text.havePropertiesChanged = true;
+        }
+    }
+
+    private void UnfocusEnemy()
+    {
+        if (focusedEnemy != null)
+        {
+            focusedEnemy.text.color = new Color(128, 128, 128, 255);
+            //focusedEnemy.text.havePropertiesChanged = true;
+            focusedEnemy = null;
+        }
+    }
+
     private void TypeLetter(string key)
     {
         if (focusedEnemy == null)
         {
-            //Goes through all the list and picks the enemy that is closer to the center of the screen
-            focusedEnemy = enemyManger.GetCloserEnemyWithName(key);
+            FocusEnemy(enemyManger.GetCloserEnemyWithName(key));
         }
-        if (focusedEnemy != null)
+        if (focusedEnemy != null && key[0] == focusedEnemy.enemyName[0])
         {
-            if (key[0] == focusedEnemy.enemyName[0])
+            focusedEnemy.enemyName = focusedEnemy.enemyName.Remove(0, 1);
+            focusedEnemy.UpdateName();
+            if (focusedEnemy.EnemyDeath())
             {
-                focusedEnemy.enemyName = focusedEnemy.enemyName.Remove(0, 1);
-                focusedEnemy.UpdateName();
-                if (focusedEnemy.EnemyDeath())
-                {
-                    focusedEnemy = null;
-                }
+                focusedEnemy = null;
             }
         }
     }
