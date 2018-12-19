8cac1cbeb3c2211a799c882fd312a7865b701040
Yessica Tamara Servín Domínguez <yessicasd@TR12.CAT>
2018-12-19 09:12:39 +0100

    adding health bar olbelisk

---
+++ TypeExorcist/Assets/Obelysk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Obelysk : MonoBehaviour {
+
+    Slider liveSlider;
+	Animator animator=null;
+	GameObject slider = null;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+        slider = transform.Find("Canvas").transform.Find("Slider").gameObject;
+       slider.SetActive(false);
+        liveSlider = slider.GetComponent<Slider>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("Lives", liveSlider.value);
+	}
+}
