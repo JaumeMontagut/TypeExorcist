9707f8830688e25a4846c917d0b6072562b501fa
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-12-24 15:24:28 +0100

    added game over screen

--- TypeExorcist/Assets/Scripts/Obelysk/Obelysk.cs
+++ TypeExorcist/Assets/Scripts/Obelysk/Obelysk.cs
@@ -1,27 +1,49 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class Obelysk : MonoBehaviour {
 
     Slider liveSlider;
 	Animator animator=null;
 	GameObject slider = null;
-    
+    GameObject GameOverPanel = null;
+    EnemyManager eManager = null;
 	void Start () {
 		animator = gameObject.GetComponent<Animator>();
         slider = transform.Find("Canvas").transform.Find("Slider").gameObject;
+        slider.SetActive(false);
         liveSlider = slider.GetComponent<Slider>();
-       
+    
+        GameOverPanel = transform.Find("Canvas").transform.Find("GameOverPanel").gameObject;
+        GameOverPanel.SetActive(false);
+        eManager = FindObjectOfType<EnemyManager>();
+
 	}
-	
+    private void Update()
+    {
+       
+    }
     public void SubstractLives(float damage)
     {
+        slider.SetActive(true);
         liveSlider.value -= damage;
         if (liveSlider.value <= 0)
         {
+            GameOverPanel.SetActive(true);
+            eManager.spawnEnemies = false;
             liveSlider.value = 0;
             animator.SetBool("Live", false);
+           
         }
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("Game");
+
     }
 }
