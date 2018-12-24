736751aaf4461346125339d9f82e02f9d8704d5a
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-12-24 13:25:24 +0100

    Removing tutorial

--- TypeExorcist/Assets/Scripts/UIManager.cs
+++ TypeExorcist/Assets/Scripts/UIManager.cs
@@ -7,80 +7,10 @@ public class UIManager : MonoBehaviour {
 
     public GameObject gamePanel;
     public GameObject helpPanel;
-    public GameObject tutorialPanel;
-
-    //Tutorial
-    public Text tutorialText;
-    public Text continueText;
-
-    public string[] tutorialLines;
-    private int currLine = 0;
-
-    private bool inTutorial = true;
-    private EnemyManager em;
-    public List<char> letters;//The letters that are going to be tested in this level
-
-    private void Start()
-    {
-        em = FindObjectOfType<EnemyManager>();
-        em.spawnEnemies = false;
-        //em.SetActive(false);
-        ShowLine();
-        //Time.timeScale = 0;
-    }
 
     private void Update()
     {
-        if (inTutorial)
-        {
-            TutorialUpdate();
-        }
-        else
-        {
-            GameUpdate();
-        }
-    }
-
-    private void TutorialUpdate()
-    {
-        //Phase 1: Introduction
-        if (Input.anyKeyDown)
-        {
-            if (currLine + 1 < tutorialLines.Length)
-            {
-                currLine++;
-                ShowLine();
-                if (currLine == tutorialLines.Length - 1)
-                {
-                    continueText.text = "Press SPACE";
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Space))
-            {
-                tutorialPanel.SetActive(false);
-                helpPanel.SetActive(true);
-            }
-        }
-        //Phase 2: Show letters
-        //TODO: Show letters above and make them turn white when they are written
-        if (helpPanel.activeInHierarchy == true)
-        {
-            for (KeyCode currKey = KeyCode.A; currKey < KeyCode.Z + 1; currKey++)
-            {
-                if (Input.GetKeyDown(currKey))
-                {
-                    letters.Remove(currKey.ToString().ToLower()[0]);
-                    if (letters.Count == 0)
-                    {
-                        //Start game
-                        inTutorial = false;
-                        em.spawnEnemies = true;
-                        helpPanel.SetActive(false);
-                        gamePanel.SetActive(true);
-                    }
-                }
-            }
-        }
+        GameUpdate();
     }
 
     private void GameUpdate()
@@ -99,10 +29,4 @@ public class UIManager : MonoBehaviour {
             }
         }
     }
-
-    private void ShowLine()
-    {
-        tutorialText.text = tutorialLines[currLine];
-    }
-
 }
