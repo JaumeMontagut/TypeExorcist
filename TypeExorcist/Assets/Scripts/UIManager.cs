767304f30f006ca27dde4e921712536b82418f47
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-12-24 03:51:17 +0100

    Adding level 01 tutorial
    Would need to be adapted for other levels, we should talk about the best way to implement it.

--- TypeExorcist/Assets/Scripts/UIManager.cs
+++ TypeExorcist/Assets/Scripts/UIManager.cs
@@ -17,9 +17,13 @@ public class UIManager : MonoBehaviour {
     private int currLine = 0;
 
     private bool inTutorial = true;
+    private GameObject em;
+    public List<char> letters;//The letters that are going to be tested in this level
 
     private void Start()
     {
+        em = FindObjectOfType<EnemyManager>().gameObject;
+        em.SetActive(false);
         ShowLine();
         //Time.timeScale = 0;
     }
@@ -38,6 +42,7 @@ public class UIManager : MonoBehaviour {
 
     private void TutorialUpdate()
     {
+        //Phase 1: Introduction
         if (Input.anyKeyDown)
         {
             if (currLine + 1 < tutorialLines.Length)
@@ -53,6 +58,26 @@ public class UIManager : MonoBehaviour {
             {
                 tutorialPanel.SetActive(false);
                 helpPanel.SetActive(true);
+            }
+        }
+        //Phase 2: Show letters
+        //TODO: Show letters above and make them turn white when they are written
+        if (helpPanel.activeInHierarchy == true)
+        {
+            for (KeyCode currKey = KeyCode.A; currKey < KeyCode.Z + 1; currKey++)
+            {
+                if (Input.GetKeyDown(currKey))
+                {
+                    letters.Remove(currKey.ToString().ToLower()[0]);
+                    if (letters.Count == 0)
+                    {
+                        //Start game
+                        inTutorial = false;
+                        em.SetActive(true);
+                        helpPanel.SetActive(false);
+                        gamePanel.SetActive(true);
+                    }
+                }
             }
         }
     }
