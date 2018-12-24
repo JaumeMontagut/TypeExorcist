a532a1862e526d967dafb7bb3d9095a80eaea31e
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-12-24 02:12:19 +0100

    Quick and easy tutorial voice lines script

---
+++ TypeExorcist/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    public Text tutorialText;
+
+    public string [] tutorialLines;
+    private int currLine = 0;
+
+    private void Start()
+    {
+        ShowLine();
+    }
+
+    private void Update () {
+        if (Input.anyKeyDown)
+        {
+            currLine++;
+            ShowLine();
+        }
+	}
+
+    private void ShowLine()
+    {
+        tutorialText.text = tutorialLines[currLine];
+    }
+}
