b78a6b231de30087d7d72dd873a9e8ab51623d72
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-12-24 02:55:06 +0100

    Making helper panel a gameobject
    Animations & rescaling are going to give us a lot of problems with UI, it's better to make it a gameobject to make it simpler to use.

--- TypeExorcist/Assets/Scripts/Tutorial.cs
+++ TypeExorcist/Assets/Scripts/Tutorial.cs
@@ -5,31 +5,5 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour {
 
-    public Text tutorialText;
-    public Text continueText;
-
-    public string [] tutorialLines;
-    private int currLine = 0;
-
-    private void Start()
-    {
-        ShowLine();
-    }
-
-    private void Update () {
-        if (Input.anyKeyDown)
-        {
-            currLine++;
-            if (currLine == tutorialLines.Length - 1)
-            {
-                continueText.text = "Press SPACE";
-            }
-            ShowLine();
-        }
-	}
-
-    private void ShowLine()
-    {
-        tutorialText.text = tutorialLines[currLine];
-    }
+   
 }
--- TypeExorcist/Assets/Scripts/UIManager.cs
+++ TypeExorcist/Assets/Scripts/UIManager.cs
@@ -8,8 +8,34 @@ public class UIManager : MonoBehaviour {
     public GameObject gamePanel;
     public GameObject helpPanel;
 
+    //Tutorial
+    public Text tutorialText;
+    public Text continueText;
+
+    public string[] tutorialLines;
+    private int currLine = 0;
+
+    private void Start()
+    {
+        ShowLine();
+        Time.timeScale = 0;
+    }
+
     private void Update()
     {
+        if (Input.anyKeyDown)
+        {
+            if (currLine + 1 < tutorialLines.Length)
+            {
+                currLine++;
+            }
+            if (currLine == tutorialLines.Length - 1)
+            {
+                continueText.text = "Press SPACE";
+            }
+            ShowLine();
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             gamePanel.SetActive(helpPanel.activeInHierarchy);
@@ -23,6 +49,11 @@ public class UIManager : MonoBehaviour {
                 Time.timeScale = 1;
             }
         }
+    }
+
+    private void ShowLine()
+    {
+        tutorialText.text = tutorialLines[currLine];
     }
 
 }
