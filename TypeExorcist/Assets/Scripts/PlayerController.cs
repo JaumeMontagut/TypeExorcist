fbc02638c0d0988a8fc26cdf562bf031d755679a
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-12-24 01:06:53 +0100

    Added dash sfx

--- TypeExorcist/Assets/Scripts/PlayerController.cs
+++ TypeExorcist/Assets/Scripts/PlayerController.cs
@@ -140,6 +140,7 @@ public class PlayerController : MonoBehaviour
             if (focusedEnemies[i].CheckDeath())
             {
                 scoreManager.Combo++;
+                gameObject.GetComponent<AudioSource>().Play();
                 anim.SetTrigger("attack");
                 trgPos.Add(focusedEnemies[i].transform.position);
                 focusedEnemies[i].DestroyEnemy();//Change for anim.settrigger die and die at the end of the animation
